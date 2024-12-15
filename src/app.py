36f8236061d2f6a31a44dee917a073f12f8332eb
= <ofcskn.omer@gmail.com>
2024-12-15 21:48:02 +0300

    Update app.py and main.py

--- src/app.py
+++ src/app.py
@@ -5,49 +5,61 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
-# Load the trained model
-model = load_model('../data/model/emotion_detector_model_1.h5')
 
-# Label map for the emotions
-label_map = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'sad', 5: 'surprise', 6: 'neutral'}
+def startApp():
+    # Load the trained model
+    model = load_model('emotion_detector_model_1.keras')
 
-# Initialize webcam
-cap = cv2.VideoCapture(0)
+    # Label map for the emotions
+    label_map = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'sad', 5: 'surprise', 6: 'neutral'}
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    # Initialize webcam
+    cap = cv2.VideoCapture(0)
 
-    if not ret:
-        print("Failed to grab frame")
-        break
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    # Convert to grayscale and resize
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    resized_frame = cv2.resize(gray_frame, (48, 48))
+        if not ret:
+            print("Failed to grab frame")
+            break
 
-    # Normalize and expand dimensions for prediction
-    resized_frame = np.expand_dims(resized_frame, axis=-1)  # Add color channel dimension
-    resized_frame = np.expand_dims(resized_frame, axis=0)  # Add batch dimension
-    resized_frame = resized_frame / 255.0  # Normalize the frame
+        # Convert to grayscale and resize
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        resized_frame = cv2.resize(gray_frame, (48, 48))
 
-    # Predict emotion
-    prediction = model.predict(resized_frame)
-    predicted_class = np.argmax(prediction)  # Get the class with the highest probability
+        # Normalize and expand dimensions for prediction
+        resized_frame = np.expand_dims(resized_frame, axis=-1)  # Add color channel dimension
+        resized_frame = np.expand_dims(resized_frame, axis=0)  # Add batch dimension
+        resized_frame = resized_frame / 255.0  # Normalize the frame
 
-    # Get the emotion label
-    emotion = label_map[predicted_class]
+        # Predict emotion
+        prediction = model.predict(resized_frame)
+        predicted_class = np.argmax(prediction)  # Get the class with the highest probability
 
-    # Display the predicted emotion on the frame
-    cv2.putText(frame, f'Emotion: {emotion}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        # Get the emotion label
+        emotion = label_map[predicted_class]
 
-    # Show the frame with emotion label
-    cv2.imshow('Emotion Detector', frame)
+        # Display the predicted emotion on the frame
+        cv2.putText(frame, f'Emotion: {emotion}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
-    # Break the loop if 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Show the frame with emotion label
+        cv2.imshow('Emotion Detector', frame)
 
-# Release the webcam and close all OpenCV windows
-cap.release()
-cv2.destroyAllWindows()
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the webcam and close all OpenCV windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+if __name__ == "__main__":
+    try:
+        # Start detection with the webcam
+        startApp()
+
+    except Exception as e:
+        print(f"An error occurred in the main program: {e}")
--- src/main.py
+++ src/main.py
@@ -5,6 +5,10 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.callbacks import EarlyStopping
+from app import startApp
+import kagglehub
 
 # Function to load and preprocess data
 def load_data(train_dir, test_dir):
@@ -30,7 +34,7 @@ def load_data(train_dir, test_dir):
         test_generator = test_datagen.flow_from_directory(
             test_dir, target_size=(48, 48), batch_size=32, color_mode="grayscale", class_mode="categorical"
         )
-
+        
         return train_generator, test_generator
     except Exception as e:
         print(f"Error loading data: {e}")
@@ -60,19 +64,23 @@ def create_model():
         raise
 
 # Train the model
-def train_model(model, train_generator, test_generator, epochs=10):
+def train_model(model, train_generator, test_generator, epochs=20):
     try:
-        model.fit(train_generator, epochs=epochs, validation_data=test_generator)
-        model.save("../data/model/emotion_detector_model_1.h5")
+        early_stop = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+        model.fit(train_generator, epochs=epochs, validation_data=test_generator, callbacks=[early_stop])
+        model.save("emotion_detector_model_1.keras")
     except Exception as e:
         print(f"Error during training: {e}")
         raise
 
 if __name__ == "__main__":
     try:
+        # Download latest version of the dataset
+        path = kagglehub.dataset_download("msambare/fer2013")
+
         # Directories for training and testing data
-        train_dir = "../data/train"
-        test_dir = "../data/test"
+        train_dir = path + "/train"
+        test_dir = path + "/test"
 
         # Load data
         train_gen, test_gen = load_data(train_dir, test_dir)
@@ -80,8 +88,14 @@ if __name__ == "__main__":
         # Create model
         model = create_model()
 
+        # Evaluate model
+        model.evaluate(test_gen)
+
         # Train model
-        train_model(model, train_gen, test_gen, epochs=10)
+        train_model(model, train_gen, test_gen, epochs=100)
+
+        # Start detection with the webcam
+        startApp()
 
     except Exception as e:
         print(f"An error occurred in the main program: {e}")
